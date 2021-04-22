4b7b7d8ecd84f1a430110fc2d69e8ef827224bc0
Kamaldeep <ksahni687@gmail.com>
2021-04-22 15:59:39 +0530

    INIT

---
+++ src/index.ts
@@ -0,0 +1,201 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import session from 'express-session';
+import passport from 'passport';
+import dotenv from 'dotenv';
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+
+import User from './user';
+import { IMongoDBUser } from './types';
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(
+  `${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log('Database Connected!');
+  }
+);
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+  session({
+    secret: 'secretcode',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user: IMongoDBUser, done: any) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser((id: string, done: any) => {
+  User.findById(id, (err: Error, doc: IMongoDBUser) => {
+    return done(null, doc);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      User.findOne(
+        { googleId: profile.id },
+        async (err: Error, doc: IMongoDBUser) => {
+          if (err) {
+            return cb(err, null);
+          }
+
+          if (!doc) {
+            // Create One
+            const newUser = new User({
+              googleId: profile.id,
+              username: profile.displayName,
+            });
+
+            await newUser.save();
+            cb(null, newUser);
+          }
+          cb(null, doc);
+        }
+      );
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: '/auth/facebook/callback',
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      User.findOne(
+        { facebookId: profile.id },
+        async (err: Error, doc: IMongoDBUser) => {
+          if (err) {
+            return cb(err, null);
+          }
+
+          if (!doc) {
+            // Create One
+            const newUser = new User({
+              facebookId: profile.id,
+              username: profile.displayName,
+            });
+
+            await newUser.save();
+            cb(null, newUser);
+          }
+          cb(null, doc);
+        }
+      );
+    }
+  )
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: '/auth/github/callback',
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      User.findOne(
+        { githubId: profile.id },
+        async (err: Error, doc: IMongoDBUser) => {
+          if (err) {
+            return cb(err, null);
+          }
+
+          if (!doc) {
+            // Create One
+            const newUser = new User({
+              githubId: profile.id,
+              username: profile.username,
+            });
+
+            await newUser.save();
+            cb(null, newUser);
+          }
+          cb(null, doc);
+        }
+      );
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  }
+);
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  }
+);
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  }
+);
+
+app.get('/get-user', (req, res) => {
+  res.send(req.user);
+});
+
+app.get('/auth/logout', (req, res) => {
+  if (req.user) {
+    req.logout();
+    res.send('done');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(5000, () => {
+  console.log(`Server listening on Port ${5000}`);
+});
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+  googleId?: string;
+  facebookId?: string;
+  githubId?: string;
+  username: string;
+}
+
+export interface IMongoDBUser {
+  googleId?: string;
+  facebookId?: string;
+  githubId?: string;
+  username: string;
+  __v: number;
+  _id: string;
+}
